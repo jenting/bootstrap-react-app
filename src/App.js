f4d5fd0869148e009536b79de487f5db2e524829
JenTing Hsiao <hsiaoairplane@gmail.com>
2023-06-11 21:53:44 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Routes, Navigate } from 'react-router-dom';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import Subpage from './Subpage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Main Page
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Navigate to='/application' />} />
+          <Route exact path="/application" render={() => <h2>Application Main Page</h2>} />
+          <Route path="/application/:name" component={Subpage} />
+        </Routes>
+
+        <Link to="/application/abc">Go to abc</Link>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/Subpage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+
+const Subpage = () => {
+  const { name } = useParams();
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            My App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      
+      <h2>AT Subpage: {name}</h2>
+      {/* Add content specific to the subpage */}
+    </div>
+  );
+};
+
+export default Subpage;
